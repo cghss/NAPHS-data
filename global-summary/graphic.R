93937ca6a381d6f486b8aa4d1d2b434947491fe7
seaneff <steph.eaneff@gmail.com>
2023-03-24 15:01:52 -0700

    update barplot graphic, all blue, remove 100% label

--- global-summary/graphic.R
+++ global-summary/graphic.R
@@ -3,7 +3,8 @@
 #############################################
 
 ## This script contains code to generate a simple barplot graphic
-## assume working directory is set to NAPHS-data
+## assume working directory is set to NAPHS-data/global-summary 
+## (this is also the source file location)
 
 #############################################
 ## Setup ####################################
@@ -21,10 +22,12 @@ library(scales) ## for commas on axes of plots
 #############################################
 
 ## NAPHS country data - one row per WHO member state
+## Data as of 
 countries <- read.delim("countries.tsv")
 
 #############################################
-## Summarize data ###########################
+## Summarize data: ##########################
+## Printed summary ##########################
 #############################################
 
 ## print summary info, globally
@@ -35,7 +38,43 @@ countries %>%
             published_naphs = sum(published_naphs == TRUE, na.rm = TRUE ),
             published_naphs_data = sum(naphs_includes_line_item_costs == TRUE, na.rm = TRUE),
             machine_readable_data = sum(naphs_data_machine_readable == TRUE, na.rm = TRUE))
-            
+
+#############################################
+## Global funnel: Manuscript barplot ########
+#############################################
+## prior interations of this barplot included below
+
+countries %>%
+  summarize(aggregation = "global",
+            completed_jee = sum(completed_jee == TRUE),
+            completed_naphs = sum(completed_naphs == TRUE),
+            published_naphs = sum(published_naphs == TRUE, na.rm = TRUE ),
+            published_naphs_data = sum(naphs_includes_line_item_costs == TRUE, na.rm = TRUE),
+            machine_readable_data = sum(naphs_data_machine_readable == TRUE, na.rm = TRUE)) %>%
+  pivot_longer(!aggregation, names_to = "metric", values_to = "count") %>%
+  mutate(label = factor(
+    recode(metric, 
+           completed_jee = "Completed JEE",
+           completed_naphs = "Completed NAPHS",
+           published_naphs  = "Published NAPHS",
+           published_naphs_data = "Published costed line items",
+           machine_readable_data = "Published machine readable data"),
+    levels = rev(c("Completed JEE", "Completed NAPHS", "Published NAPHS", "Published costed line items", "Published machine readable data")))) %>%
+  mutate(pct = count/max(count)) %>% 
+  ggplot(aes(x = count, y = label, fill = label)) +
+  geom_bar(stat = "identity", color = "black", fill = "#172869") +
+  geom_text(aes(label = paste(count, 
+                              ifelse(pct != 1,
+                                     paste(" (", round(pct*100, 0), "%)", sep = ""),
+                                     paste("")))),
+            hjust = 0, nudge_x = 4, size = 3) +
+  ylab("") +
+  xlab("Number of Countries\n(IHR member States)") +
+  xlim(0, 108) +
+  theme_minimal() + 
+  theme(plot.caption = element_text(size = 7)) +
+  ggtitle("Participation in Monitoring and Evaluation\nFramework Process")
+
 #############################################
 ## Global funnel: barplot (v1) ##############
 #############################################
